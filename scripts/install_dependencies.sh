343425a72a6b6a05b92bb43d1319e7c937d02d4c
Peter Kiptanui <37255906+collengine@users.noreply.github.com>
2025-10-05 23:47:32 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Installing dependencies and setting up application..."
 # Create application directory if it doesn't exist
-mkdir -p /var/www/docbooker
+#mkdir -p /var/www/docbooker
 # Set proper ownership and permissions
-chown -R nginx:nginx /var/www/docbooker
+chown -R ec2-user:ec2-user /var/www/docbooker
 chmod -R 755 /var/www/docbooker
 #cp -rf .next/static/*  /var/www/docbooker
 #cp -rf .next/standalone/*  /var/www/docbooker
