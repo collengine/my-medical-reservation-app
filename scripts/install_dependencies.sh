cd49cdb55b26aa0d716cd9cfc719be09870880b4
Peter Kiptanui <37255906+collengine@users.noreply.github.com>
2025-10-06 01:54:09 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,8 +5,8 @@ echo "Installing dependencies and setting up application..."
 # Set proper ownership and permissions
 chown -R ec2-user:ec2-user /var/www/docbooker
 chmod -R 755 /var/www/docbooker
-#cp -rf .next/static/*  /var/www/docbooker
-#cp -rf .next/standalone/*  /var/www/docbooker
+yes | cp -rf .next/static/*  /var/www/docbooker
+yes | cp -rf .next/standalone/*  /var/www/docbooker
 # Install Node.js if not present
 if ! command -v node &> /dev/null; then
     yum install -y nodejs npm
